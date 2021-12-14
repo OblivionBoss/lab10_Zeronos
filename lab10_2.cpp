9ed70e8f00b9f5923a9d4956204963f79524b19f
Zeronos <boss16805@gmail.com>
2021-12-14 18:18:54 +0700

    lab10_2 completed

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 //Write the function printO() here
+char printO(int x,int y){
+	if(x <= 0 or y <= 0){
+		cout << "Invalid input";
+	}
+	else if(y > x or y < x ){
+		for(int i = 0; i < x ; i++){
+			for(int j = 0 ; j < y ; j++){
+				cout << 'O';
+			}
+			cout <<'\n';
+		}
+	}
+	else if(x == y){
+		for(int i = 0; i < x ; i++){
+			for(int j = 0 ; j < y ; j++){
+				cout << 'O';
+			}
+			cout <<'\n';
+		}
+	}
+	return 0;
+}
 
 int main(){
 	
